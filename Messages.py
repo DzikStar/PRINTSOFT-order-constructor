fc083716a66b9e90c3a3e01d4ad849e42e0eef7d
Dawid Mila <miladawid@printsoft.pl>
2024-11-07 13:24:21 +0100

    specs strings

--- Messages.py
+++ Messages.py
@@ -1,4 +1,4 @@
-ASK_USER_OFFER_TYPE = """Typy Ofert
+PROMPT_USER_OFFER_TYPE = """Typy ofert
 1.) Komputer
 2.) Laptop
 3.) Monitor
@@ -6,4 +6,23 @@ ASK_USER_OFFER_TYPE = """Typy Ofert
 5.) Inna
 
 Wybierz: """
+
+PROMPT_USER_AMOUT = "Wybierz ilość"
+SPEC_PRODUCENT = "Producent"
+SPEC_PROCESSOR = "Processor"
+SPEC_RAM = "Pamięć RAM"
+SPEC_DISK = "Dysk"
+SPEC_OS = "System"
+SPEC_WARRANTY = "Gwarancja"
+SPEC_MONITOR_SIZE = "Rozmiar matrycy"
+SPEC_RETAIL_PRICE = "Cena detaliczna"
+SPEC_RESOLUTION = "Rozdzielczość"
+SPEC_DISPLAY_TYPE = "Typ matrycy"
+SPEC_DISPLAY_HZ = "Częstotliwość odświeżania"
+SPEC_PRINT_TYPE = "Typ Druku"
+SPEC_DUPLEX = "DUPLEX"
+SPEC_PRINT_COLOR = "Kolor druku"
+SPEC_COMMUNICATION = "Komunikacja"
+SPEC_SCANNER = "Skaner"
+SPEC_MISC = "Inne"
 
